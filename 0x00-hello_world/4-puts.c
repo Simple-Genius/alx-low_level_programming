8754423560fe9d5feea72151c48aaac340f71a04
Simple-Genius <ajila.adeboye@gmail.com>
2023-03-16 13:10:22 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 
 /**
- *   This is to print the value of str using 'puts'
+ *  
+ *	This is to print the value of str using 'puts'
+ *
+ *	prints the value of str
+ *
+ *	returns '0'
  */
-int main()
+int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(str);
 	return (0);
 }
