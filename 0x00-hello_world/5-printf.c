8a425c2efa4b21b05d6adab1e1c75bc282b0df06
Simple-Genius <ajila.adeboye@gmail.com>
2023-03-16 14:58:13 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - The program prints a certain string
+ *
+ * A certain string is printed using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
