08067b342e4c7169ba534638097e4f293e7d2714
Simple-Genius <ajila.adeboye@gmail.com>
2023-03-21 12:45:01 -0700

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,17 @@ int print_sign(int n);
 
 int print_sign(int n)
 {
+	
 	if (n > 0)
 	{
-		putchar('+');
-		return (1);
+		return (+1);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
-		putchar('-');
 		return (-1);
 	}
 	else
 	{
-		putchar(0);
 		return 0;
 	}
 }
