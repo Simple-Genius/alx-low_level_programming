b82be7911f63cda6094fea3dc78ab24848e6dd51
Simple-Genius <ajila.adeboye@gmail.com>
2023-03-21 14:57:28 -0700

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ *add - function to add two numbers
+ *
+ * @num1:the number to be added
+ * @num2: the second number to be added
+ * Return: sum
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
