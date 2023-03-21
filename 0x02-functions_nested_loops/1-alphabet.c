32768427a0fe51b2ab1a5e143f9e32a20853bea7
Simple-Genius <ajila.adeboye@gmail.com>
2023-03-21 09:03:55 -0700

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,13 +9,14 @@ void print_alphabet(void);
 
 int main(void)
 {
-	print_alphabet()
+	print_alphabet();
         return (0);
 }
 
 void print_alphabet(void)
 {
 	char a;
+
 	for (a = 'a'; a <= 'z'; a++)
 	{
 		putchar(a);
