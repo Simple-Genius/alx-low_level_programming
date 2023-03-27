2693e73cef525747d7cfb13154eb105ea4dfd10a
Simple-Genius <ajila.adeboye@gmail.com>
2023-03-27 12:06:47 -0700

    Done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,18 +13,18 @@
 void rev_string(char *s)
 {
 	char temp;
-    	int i = 0, j = 0;
-    	while (s[j] != '\0')
+	int i = 0, j = 0;
+	while (s[j] != '\0')
 	{
-        	j++;
-    	}
-    	j--;
-    	while (i < j)
+		j++;
+	}
+	j--;
+	while (i < j)
 	{
-        	temp = s[i];
-        	s[i] = s[j];
-        	s[j] = temp;
-        	i++;
-        	j--;
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
 	}
 }
