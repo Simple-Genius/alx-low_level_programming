adc7a0f39d46ebbf263540db4a90489958d338b0
Simple-Genius <ajila.adeboye@gmail.com>
2023-03-27 06:59:28 -0700

    Done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * _puts - a function that prints a string, followed by a new line, to stdout
  * 
@@ -9,5 +10,6 @@
  */
 void _puts(char *str)
 {
-	puts(*str);
+	const char b = *str;
+	puts(b);
 }
