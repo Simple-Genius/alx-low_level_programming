fca254330e7a831bd9fe2bb749d93b596449b903
Simple-Genius <ajila.adeboye@gmail.com>
2023-03-27 13:56:09 -0700

    Done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,11 @@
  *
  * Return: Always 0
  */
-void puts2(char *str)
+void putis2(char *str)
 {
 	int i = 0;
-	while (str[i] != '\0')
+
+	while (str[i] != '\0'|| str[i + 1] != '\0')
 	{
 		putchar(str[i]);
 		i += 2;
