3584e4040028bbd1c27917369bf49be97044d693
Simple-Genius <ajila.adeboye@gmail.com>
2023-03-29 01:13:01 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *
+ *
+ *@
+ *
+ * Return: Always 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *join;
+	join = strcat(dest,src);
+	return join;
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a readme for the 0x06-pointers, arrays, strings project.
