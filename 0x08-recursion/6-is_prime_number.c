cca2bd643b22747d24e5a71035ba4ddf116745d6
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-05 16:10:38 -0700

    DONE

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,5 @@
 /**
- * _sqrt_recursion - a function that returns 1 if the 
+ * is_prime_number - a function that returns 1 if the 
  * input integer is a prime number, otherwise return 0.
  * 
  *
@@ -15,11 +15,11 @@ int is_prime_number(int n)
 	if(n % 2 == 0 || n % 3 == 0 || 
 	n % 5 == 0 || n % 7 == 0 || n % 11 == 0 || n == 1 || n == -1)
 	{
-		return 0;
+		return (0);
 	}
 	if (n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || i == 1)
 	{
-		return 1;
+		return (1);
 	}
-	return i;
+	return (i);
 }
