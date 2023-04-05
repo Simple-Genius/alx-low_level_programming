f2dc72077bc62c5d9094a27a688f22cc7d5ea0ca
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-05 16:12:30 -0700

    DONE

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int is_prime_number(int n)
 {
 	int i = 1;
 
-	if(n % 2 == 0 || n % 3 == 0 ||
+	if (n % 2 == 0 || n % 3 == 0 ||
 	n % 5 == 0 || n % 7 == 0 || n % 11 == 0 || n == 1 || n == -1)
 	{
 		return (0);
