79cd59f435f4b26ad27b6ed21e1e27fde1fa4c1d
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-11 10:05:32 -0700

    DONE

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,6 +20,8 @@ char *create_array(unsigned int size, char c)
 	text = malloc(sizeof(char) * size);
 	if (size == 0)
 		return (NULL);
+	if (text == 0)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
