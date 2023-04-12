1eb1b07e1b5826612659ffc3b0c9038867cf954a
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-12 03:45:22 -0700

    DONE

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,25 +1,40 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
-#include <string.h>
-
 
 /**
- * str_concat - a function that concatenates two strings
- * 
+ * str_concat - Concatenates two strings.
+ * @s1: The string to be concatenated.
+ * @s2: The string to be concatenated.
  *
- *@s1: string parameter
- *@s2: other string parameter
- * Return: Always 0
+ * Return: concatenated string or null depending on situation
  */
-
 
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
-	int a, b;
-	a = strlen(s1);
-	b = strlen(s2);
-	s3 = malloc(sizeof(char) * (a + b));
-	s3 = strcat(s1,s2);
-	return (s3);
+	char *joinedString; //concatString
+	int indexA; //index
+	int indexB = 0; //concatIndex
+	int length = 0; //len
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (indexA = 0; s1[indexA] || s2[indexA]; indexA++)
+		length++;
+
+	joinedString = malloc(sizeof(char) * len);
+
+	if (joinedString == NULL)
+		return (NULL);
+
+	for (indexA = 0; s1[indexA]; indexA++)
+		joinedString[indexB++] = s1[indexA];
+
+	for (indexA = 0; s2[indexA]; indexA++)
+		joinedString[indexB++] = s2[indexA];
+
+	return (joinedString);
 }
