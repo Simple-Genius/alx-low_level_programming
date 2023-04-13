3e6e8def1f67cf25805200b793969f2c6c75ff0e
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-13 13:33:31 -0700

    DONE

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 /**
  * string_nconcat - a function that concatenates two strings.
- *
+ * 
  *
  *@s1: string parameter
  *@s2: other string parameter
@@ -15,29 +16,30 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *joinedString;
-	int size1, size2;
-
-	if (s1 == "" || s2 == "" || n == 0)
-	{
-		return (NULL);
-	}
-
+	int i = 0, j= 0, size1 = 0, size2 = 0;
+        
 	size1 = strlen(s1);
 	size2 = strlen(s2);
-
-	if (n >= size2)
+       
+	if (n >= size2)	
 	{
-		joinedString = malloc(sizeof(char) * ((size1 + size2 + 1)));
-		if (joinedString == NULL)
-		{
-			return (NULL);
-		}
-		return (joinedString);
+		n = size2;
 	}
-
-
-	strncat(s1, s2, n);
-	putchar(joinedString);
-
-	return (joinedString);
+        
+	joinedString = malloc(sizeof(char) * (size1 + size2 + 1));
+        
+        
+	while (i <= size1)
+	{
+		joinedString[i] = s1[i];
+		i++;
+	}
+        
+	while (j <= n)
+	{
+		joinedString[j + size1] = s2[j];
+		j++;
+	}
+        
+	return joinedString;
 }
