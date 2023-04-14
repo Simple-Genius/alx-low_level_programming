95a2e17e50b334ab4a8f48e2102c152cf67a32a3
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-14 03:43:53 -0700

    DONE

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = size2;
 	}
 
-	joinedString = malloc(sizeof(char) * (size1 + n + 1));
+	joinedString = malloc(size1 + n + 1);
 
 	if (joinedString == NULL)
 	{
