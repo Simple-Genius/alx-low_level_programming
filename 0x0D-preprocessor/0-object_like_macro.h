af5b49e56e313e8b2137befc37fef5afc8aa5d19
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-17 11:37:57 -0700

    DONE

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,8 +1,7 @@
-#ifndef "0-object_like_macro.h"
-#define "0-object_like_macro.h"
-#include "0-object_like_macro.h"
+#ifndef 0-OBJECT_LIKE_H
+#define 0-OBJECT_LIKE_H
 #define SIZE 1024
 
 
 
-#endif /*0-object_like_macro.h*/
+#endif
