67f295c6a115ce7b622a0bd1e33b21fb88ec7fea
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-18 10:33:31 -0700

    DONE

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - Function that prints 
+ * the parameters of the struct
+ * 
+ *@d - The struct parameter
+ *
+ * Return: Always 0
+ */
+
+void print_dog(struct dog *d)
+{
+    printf("%s\n", d->name);
+    printf("%f\n", d->age);
+    printf("%s\n", d->owner);
+}
+
