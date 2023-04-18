9f0ff914f94af81f9912025ee660dbd51d5314d3
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-18 04:26:48 -0700

    DONE

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct my_struct - A struct named dog
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's Owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
