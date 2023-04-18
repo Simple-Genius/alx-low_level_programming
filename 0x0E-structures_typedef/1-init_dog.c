1a7f8a56f4e3cb229ebb2647840892233840dd94
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-18 09:34:51 -0700

    DONE

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,9 +14,9 @@
  *
  * Return: Always 0
  */
-void init_dog(struct dog *d, char *name, float age, char *owner){
-	d = malloc(sizeof(struct dog));
-	strcpy(d->name,name);
-	strcpy(d->owner,owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->owner = owner;
+	d->name = name;
 	d->age = age;
 }
