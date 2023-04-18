478af59245a25242f41672bab3920b25e1cd95a1
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-18 06:24:37 -0700

    DONE

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,7 +14,7 @@
  * Return: Always 0
  */
 
-void init_dog (struct dog *d, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 d->name = name;
 d->age = age;
