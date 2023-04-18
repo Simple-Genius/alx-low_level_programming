5bd17f692d93910176ab61d527e9e8aaae2e9c62
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-18 06:23:44 -0700

    DONE

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,7 +4,7 @@
 
 /**
  * init_dog - A function to assign values to struct
- * 
+ *
  *
  *@d: the struct parameter
  *@name: the name of the dog
@@ -14,8 +14,8 @@
  * Return: Always 0
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner){
-    
+void init_dog (struct dog *d, char *name, float age, char *owner)
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
