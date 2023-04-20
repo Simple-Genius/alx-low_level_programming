f2e2329cc6f81b8268c249923114635ab242f7ef
“Simple-Genius” <ajila.adeboye@gmail.com>
2023-04-20 00:03:33 -0700

    DONE

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,13 +15,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-long unsigned int i = 0;
+unsigned long int i = 0;
 
 if (size == 0)
-        return;
+	return;
 for (i = 0; i < size; i++)
 {
-if (action == NULL|| array == NULL)
+if (action == NULL || array == NULL)
 	return;
 action(array[i]);
 }
