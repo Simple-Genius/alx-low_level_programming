b33c9a034b31f9590301cde9aeec3e9c94847b34
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-28 03:56:05 +0100

    DONE

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,13 +16,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 
+		printf("[%d] ", h->len);
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] ", h->len);
 			printf("%s\n", h->str);
 		}
 
