43bbf6ea31db4dc0013a304a195067cf98fc1cae
Simple-Genius <ajila.adeboye@gmail.com>
2023-04-28 03:40:06 +0100

    DONE

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - A function that frees all elements in a linked list
+ * @head: pointer to head element of list
+ * Return: Nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	ptr = head;
+	while (head)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr->str);
+		free(ptr);
+	}
+	free(head);
+}
