4419be8620554ce76ee0ae5c32a0e8d126f2e67e
Simple-Genius <ajila.adeboye@gmail.com>
2023-05-01 06:35:03 +0100

    DONE

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,51 +1,44 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-/**
- * string_nconcat - a function that concatenates two strings.
- *
- *
- *@s1: string parameter
- *@s2: other string parameter
- *@n: the number of bytes
- *
- * Return: Always 0
- */
 
+/**
+ * string_nconcat - A function that concatenates two strings
+ * @s1: An input pointer of the first string
+ * @s2: An input pointer of the second string
+ * @n: an input integer of number of string to concatenate
+ * Return: Apointer to concatened strings or NULL if it str is NULL
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *joinedString;
-	unsigned int i = 0, size1 = 0, size2 = 0;
+	char *new_str;
+	unsigned int i = 0, lens1 = 0, lens2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 
+	while (s1[lens1])
+		lens1++;
+
 	if (s2 == NULL)
 		s2 = "";
 
-	size1 = strlen(s1);
-	size2 = strlen(s2);
+	while (s2[lens2])
+		lens2++;
 
-	if (n >= size2)
-	{
-		n = size2;
-	}
+	if (n >= lens2)
+		n = lens2;
 
-	joinedString = malloc(size1 + n + 1);
-
-	if (joinedString == NULL)
-	{
+	new_str = malloc(lens1 + n + 1);
+	if (new_str == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < (size1 + n); i++)
+	for (; i < (lens1 + n); i++)
 	{
-		if (i < size1)
-			joinedString[i] = *s1, s1++;
+		if (i < lens1)
+			new_str[i] = *s1, s1++;
 		else
-			joinedString[i] = *s2, s2++;
+			new_str[i] = *s2, s2++;
 	}
-
-	return (joinedString);
+	new_str[i] = '\0';
+	return (new_str);
 }
