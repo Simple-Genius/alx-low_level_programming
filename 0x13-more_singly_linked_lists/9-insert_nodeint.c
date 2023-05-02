fe1ab64bf4aa23f70b1115567dcfa7c79ac62521
Simple-Genius <ajila.adeboye@gmail.com>
2023-05-02 08:40:13 +0100

    DONE

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr, *trv, *count;
 	unsigned int l = 1, j = 0;
 
+	if (idx < j)
+		return (NULL);
+
 	count = *head;
 	while (count)
 	{
@@ -26,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		count = count->next;
 	}
 
-	if (idx > j || idx < 0)
+	if (idx > j)
 		return (NULL);
 
 	ptr = malloc(sizeof(listint_t));
